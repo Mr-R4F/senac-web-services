fea8349d13969a81f372c37abd1694a6ad15616f
RAFAEL MARTINS SOARES <rafael.msoares5@senacsp.edu.br>
2023-04-10 22:34:10 -0300

    CRUD avançado

--- Senac-SenacBikeStore/app/Http/Controllers/api/CategoriaController.php
+++ Senac-SenacBikeStore/app/Http/Controllers/api/CategoriaController.php
@@ -6,16 +6,24 @@ use App\Http\Resources\CategoriaResource;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCategoriaRequest;
 use App\Models\Categoria;
-//use Illuminate\Http\Request;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class CategoriaController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request) //ORDERNAÇÃO
     {
-        $categorias = Categoria::all(); //chama método estático
+        $sortParameter = $request->input('ordernacao', 'nome_da_categoria');
+        $sortDirection = Str::startsWith($sortParameter, '-') ? 'DESC' : 'ASC';
+        $sortColumn = ltrim($sortParameter);
+
+        $categorias = $sortColumn === 'nome_da_categoria' ? Categoria::orderBy('nomedacategoria', $sortDirection)->get() : Categoria::all();
 
         return response()->json([
             'Status' => 200,
@@ -50,9 +58,67 @@ class CategoriaController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Categoria $categoria)
+    public function show($categoriaId)
     {
-        //
+        try { //código que consome o recurso
+
+            //validação de entrada para ter certeza que o valor é numérico
+            $validator = Validator::make([
+                'id' => $categoriaId
+            ],[
+                'id' => 'integer'
+            ]);
+
+            if($validator->fails()) {
+                throw ValidationException::withMessages([
+                    'id' => 'O campo id deve ser númerico'
+                ]);
+            }
+
+            $categoria = Categoria::findorFail($categoriaId); //fizemos o que o web services entrega
+            //tratamentos de exeção erros.//para corrigir ou mitigar (evitar o contornar)
+           // porque se tem ? pois na computação há recursos que se vai usar mais são recursos escasso (conexão, arquivo em disco, memoria, processamento) n]ao necessáriamente tem recurso para ele
+            //no caso de falha pra a falta de recurso (tenta-se evitar um probelma)
+
+            //antecipação para algo não disponivel
+            //deixa lenta
+
+            return response()->json([
+                'status' => 200,
+                'mensagem' => 'Categoria retornada',
+                'categoria' => new CategoriaResource($categoria)
+            ], 200);
+        } catch (\Throwable $err) { //em caso de falha vem para cá
+            $class = get_class($err);
+
+            switch ($class) { //verifica o tipo da exeção
+                case ModelNotFoundException::class:
+                    return response()->json([
+                        'status' => 200,
+                        'mensagem' => 'Categoria retornada',
+                        'categoria' =>[]
+                    ], 200);
+                    break;
+
+                case \Illuminate\Validation\ValidationException::class:
+                    return response()->json([
+                        'status' => 406,
+                        'mensagem' => $err->getMessage(),
+                        'categoria' => []
+                    ], 406);
+                    break;
+
+                default: //coloca um padrão caso não for esse erros
+                    return response()->json([
+                        'status' => 500,
+                        'mensagem' => 'Erro interno',
+                        'categoria' => []
+                    ], 500);
+                    break;
+            }
+        }
+        //uso paRA MANIPULAR recurso possivel de falha
+        //no win não há trycatch (tela azul)
     }
 
     /**
--- Senac-SenacBikeStore/app/Http/Controllers/api/ProdutoController.php
+++ Senac-SenacBikeStore/app/Http/Controllers/api/ProdutoController.php
@@ -5,17 +5,57 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProdutoResource;
 use App\Http\Requests\StoreProdutoRequest;
+use App\Http\Resources\CategoriaResource;
 use App\Models\Produto;
-
-//use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class ProdutoController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    /*
+    public function index(Request $request) //PAGINAÇÃO
     {
+        $input = $request->input('pagina'); //captura a entrada
+
+        $query = Produto::with('categoria', 'marca'); //traz categoria e marca junto a paginação
+
+        if($input) {  //se manduo algo 
+            $page = $input;//recebe a página definida pelo user
+            $perPage = 10; //qtd por página
+            $query->offset(($page - 1) * $perPage)->limit($perPage);
+            $produtos = $query->get();
+
+            $recordsTotal = Produto::count();//conta os elementos do array
+            $numberOfPages = ceil($recordsTotal / $perPage);
+            $response = response()->json([
+                'status' => 200,
+                'mensagem' => 'Lista de produtos retornada',
+                'produos' => ProdutoResource::collection($produtos),
+                'meta' => [
+                    'total_numero_de_registros' => (string) $recordsTotal,
+                    'numero_de_registro_por_pagina' =>  (string) $recordsTotal,
+                    'numero_de_paginas' => (string) $numberOfPages,
+                    'pagina_atual' => $page
+                ]
+            ], 200);
+        } else {
+            $produtos = $query->get();
+
+            $response = response()->json([
+                'status' => 200,
+                'mensagem' => 'Lista de produtos retornada',
+                'produos' => ProdutoResource::collection($produtos)
+            ], 200);
+
+            return $response;
+        }
+
         $produtos = Produto::all(); //chama método estático
 
         return response()->json([
@@ -23,6 +63,152 @@ class ProdutoController extends Controller
             'Mensagem' => 'Lista de produtos retornadas',
             'Categorias' => ProdutoResource::collection($produtos) //pega o json do usuário
         ], 200);
+
+        //ordernar 
+        //paginar (pegar bloco)
+        //filtrar pegar algo especifico (evita stress do banco)
+        //nome e val do campo
+    } */
+
+    /* public function index(Request $request) //FILTRO
+    {
+        //trabalha com o filtro de entrada
+        $query = Produto::with('categoria', 'marca');
+
+        $filterParameter = $request->input('filtro');
+
+        if($filterParameter === null) {
+            //retorna todos os produtos   
+            $produtos = $query->get();
+
+            $response = response()->json([
+                'status' => 200,
+                'mensagem' => 'Listagem de produtos retornada',
+                'produtos' => ProdutoResource::collection($produtos)
+            ], 200);
+            return $response;
+        } else {
+            //obtém o nome do filtro e o criterio
+            [$filteCriteria, $filterValue] = explode(':', $filterParameter);
+            if($filteCriteria === 'nome_da_categoria') {
+                //faz inner join para obter a categoria
+
+                $produtos = $query->join('categorias', 'pkcategoria', '=', 'fkcategoria')
+                    ->where('nomedacategoria', '=', $filterValue)->get();
+                
+                $response = response()->json([
+                    'status' => 200,
+                    'mensagem' => 'Listagem de produtos retornada',
+                    'produtos' => ProdutoResource::collection($produtos)
+                ], 200);
+                return $response;
+            } else {
+                //usuário chamou um filtro que não existe  
+                $response = response()->json([
+                    'status' => 406,
+                    'mensagem' => 'Filtro não aceita',
+                    'produtos' => []
+                ], 406);
+                return $response;
+            }
+        }
+    } */
+
+    public function index(Request $request) //TUDO JUNTO
+    {
+        //trabalha com o filtro de entrada
+        $query = Produto::with('categoria', 'marca');
+        $mensagem = 'Lista de produtos retornada';
+        $codigoDeRetorno = 0;
+
+        //realiza o processamento do filtro
+        //Obtém o parametro do filtro
+        $filterParameter = $request->input('filtro');
+
+        if($filterParameter === null) {
+            //retorna todos os produtos & Default
+            $mensagem = 'Lista de produtos retornada - completa';
+            $codigoDeRetorno = 200;
+        } else {
+            //obtém o nome do filtro e o criterio
+            [$filteCriteria, $filterValue] = explode(':', $filterParameter);
+
+            //se o filtro está adequado
+            if($filteCriteria === 'nome_da_categoria') {
+                //faz inner join para obter a categoria
+
+                $produtos = $query->join('categorias', 'pkcategoria', '=', 'fkcategoria')
+                    ->where('nomedacategoria', '=', $filterValue)->get();
+
+                $mensagem = 'Lista de produtos retornada - filtrada';
+                $codigoDeRetorno = 200;
+            } else {
+                //usuário chamou um filtro que não existe, então não há nada a retornar (erro 406 - not  accepted)
+               $produtos = [];
+               $mensagem = 'Filtro não aceito';
+               $codigoDeRetorno = 200;
+            }
+        }
+
+        if($codigoDeRetorno === 200) {
+            //realiza o processamento da ordenação
+            $sorts = explode(',', $request->input('ordenacao', ''));
+
+            foreach ($sorts as $sortColumn) {
+                $sortDirection = Str::startsWith($sortColumn, '-') ? 'DESC' : 'ASC';
+                $sortColumn = ltrim($sortColumn, '-');
+
+                switch ($sortColumn) {
+                    case 'nome_da_produto':
+                        $query->orderBy('nomedoproduto', $sortDirection);
+                        break;
+
+                    case 'ano_do_modelo':
+                        $query->orderBy('anodomodelo', $sortDirection);
+                        break;
+
+                    case 'preco_da_lista':
+                        $query->orderBy('precodalista', $sortDirection);
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+            $mensagem = $mensagem . '+Ordenada';
+        }
+
+        //realiza o processamento da paginação
+
+        $input = $request->input('pagina');
+
+        if($input) {
+            $page = $input;
+            $perPage = 10;
+            $query->offset(($page - 1) * $perPage)->limit($perPage);
+            $produtos = $query->get();
+
+            $recordsTotal = Produto::count();
+            $numberOfPages = ceil($recordsTotal / $perPage);
+            $mensagem = $mensagem . '+Paginada';
+        }
+
+        if($codigoDeRetorno === 200) {
+            $produtos = $query->get();
+
+            $response = response()->json([
+                'status' => 200,
+                'mensagem' => $mensagem,
+                'produtos' => ProdutoResource::collection($produtos)
+            ], 200);
+        } else {
+            $response = response()->json([
+                'status' => 406,
+                'mensagem' => $mensagem,
+                'produtos' => $produtos
+            ], 406);
+        }
+        return $response;
     }
 
     /**
@@ -54,9 +240,9 @@ class ProdutoController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Produto $produto)
+    public function show($categoriaId)
     {
-        //
+      
     }
 
     /**
